95b10528d696919adde1dff25abce218b26c7eb4
lavanyaventrapragada9-eng <lavanyaventrapragada9@gmail.com>
2026-07-01 00:07:50 +0530

    User Render environment variables

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import mysql.connector
-
+import os
 # MySQL Connection
    
 def get_connection():
     return mysql.connector.connect(
-        host=st.secrets["DB_HOST"],
-        user=st.secrets["DB_USER"],
-        password=st.secrets["DB_PASSWORD"],
-        database=st.secrets["DB_NAME"],
-        port=st.secrets["DB_PORT"]
+        host=os.getenv["DB_HOST"],
+        user=os.getenv["DB_USER"],
+        password=os.getenv["DB_PASSWORD"],
+        database=os.getenv["DB_NAME"],
+        port=int(os.getenv)["DB_PORT"]
     )
         
     
